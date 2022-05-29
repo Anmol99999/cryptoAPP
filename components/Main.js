8123711db51f8c94b5f6168fa44dfd7207a1cead
Anmol <anmol@anmol.com>
2022-05-29 05:51:25 +0530

    first commit

---
+++ components/Main.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Portfolio from './Portfolio'
+import styled from 'styled-components'
+import Promos from './Promos'
+
+const Main = ({walletAddress,
+  sanityTokens,
+  thirdWebTokens}) => {
+  return (
+   <Wrapper>
+       <Portfolio walletAddress={walletAddress}
+              sanityTokens={sanityTokens}
+              thirdWebTokens={thirdWebTokens}/>
+       <Promos/>
+   </Wrapper>
+  )
+}
+
+export default Main
+const Wrapper = styled.div`
+  display: flex;
+  max-height: calc(100vh - 64px);
+  overflow: hidden;
+  overflow-y: scroll;
+  ::-webkit-scrollbar{
+    display:none;
+  }
+  & div {
+    border-radius: 0.4rem;
+  }
+`
